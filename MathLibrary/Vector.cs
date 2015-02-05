0ed03cfe0a3420e336f6f74c2a037ba3c72868ee
villekk <ville.kurkela@seamk.fi>
2015-02-05 18:28:15 +0200

    Vektoritiedostoa muokattu

--- MathLibrary/Vector.cs
+++ MathLibrary/Vector.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 namespace MathLibrary
 {
     class Vector
-    {
+    {   //Vektorilaskentaa
         public double X { get; set; }
         public double Y { get; set; }
         public double Z { get; set; }
-
+        //Konstruktori
         public Vector(double x, double y, double z)
         {
             this.X = x;
